09fbe2f96adf8e0c1fc54a8152457f5c664335c6
Getro Naissance and Michelle Llaguno <michelle@>
2016-10-20 17:12:36 -0700

    project is complete

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("form#triangles").submit(function(event){
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    if (!side1 || !side2 || !side3) {
+      $('#result4').show();
+    } else if (side1 === side2 && side1 === side3){
+      $('#result1').show();
+    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+      $('#result2').show();
+    } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+      $('#result3').show();
+    }
+    event.preventDefault();
+  });
+});
